255a6cbb8b392424c85e7bb5669a2ab44124c7b6
zeenayouhan <zinayouhan33@gmail.com>
2020-05-06 09:54:31 +0530

    initial

--- NestjsCRUD/nest-js-crud/src/app.module.ts
+++ NestjsCRUD/nest-js-crud/src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 
 @Module({
-  imports: [ProductModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [],
+ 
 })
 export class AppModule {}
